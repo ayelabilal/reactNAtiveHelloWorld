0aa22e5dcc4da301338dcf85cdaa53c9993cd68a
ayelabilal <ayelabilal944@gmail.com>
2024-10-12 19:07:35 +0500

    cmplt

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:60,color:"blue",display:"flex"}}>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
